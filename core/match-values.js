3dac7cf552b78b2a8791877e098bf02e37ae9a9c
golyshevd <golyshev.dmitry@yandex.ru>
2015-03-18 06:02:31 +0300

    Stylish refactor matchValues

--- core/match-values.js
+++ core/match-values.js
@@ -1,53 +1,48 @@
 'use strict';
 
-function Alternate(ruleIndex, valueIndex) {
+function Path(ruleIndex, valueIndex) {
     this.ruleIndex = ruleIndex;
     this.valueIndex = valueIndex;
     this.value = null;
 }
 
-function matchValues(rules, types, values) {
-    /*eslint
-        max-depth: 0,
-        no-constant-condition: 0,
-        complexity: 0
-        */
-    var result = [];
-    var alters = [];
-    var valuesLength = values.length;
-    var rulesLength = rules.length;
-    var matchLength = 0;
-    var ruleIndex = 0;
-    var matchCount = 0;
-    var matchIndex = -1;
-    var valueIndex = matchIndex;
-
+function matchValues(rules, kinds, value) {
+    /*eslint max-depth: 0, no-constant-condition: 0, complexity: 0*/
+    var kind = void 0;
     var path = null;
     var rule = void 0;
-    var type = void 0;
+    var match = [];
+    var paths = [];
+    var matchCount = 0;
+    var matchIndex = -1;
+    var rulesIndex = 0;
+    var valueIndex = matchIndex;
+    var valueLength = value.length;
+    var rulesLength = rules.length;
+    var matchLength = 0;
 
     do {
-        rule = rules[ruleIndex];
-        type = types[rule.kind];
+        rule = rules[rulesIndex];
+        kind = kinds[rule.kind];
         matchCount = 0;
         valueIndex = matchIndex;
 
-        while (valuesLength - valueIndex > 1) {
+        while (valueLength - valueIndex > 1) {
             valueIndex += 1;
             // find next matched value
-            if (!type.check(values[valueIndex])) {
+            if (!kind.check(value[valueIndex])) {
                 continue;
             }
 
             matchCount += 1;
-            matchLength = result.push(values[valueIndex]) - 1;
+            matchLength = match.push(value[valueIndex]) - 1;
 
             if (rule.required) {
                 if (matchCount > 1) {
-                    alters[matchLength] = new Alternate(ruleIndex, matchIndex);
+                    paths[matchLength] = new Path(rulesIndex, matchIndex);
                 }
             } else {
-                path = alters[matchLength] = new Alternate(ruleIndex, matchIndex);
+                path = paths[matchLength] = new Path(rulesIndex, matchIndex);
 
                 if (matchCount === 1) {
                     path.value = rule.value;
@@ -71,7 +66,7 @@ function matchValues(rules, types, values) {
         if (!rule.required) {
             if (rule.value) {
                 // push default value if rule is not required
-                result.push(rule.value);
+                match.push(rule.value);
             }
             continue;
         }
@@ -79,24 +74,26 @@ function matchValues(rules, types, values) {
         path = null;
 
         // back to alternate path
-        while (result.pop()) {
-            matchLength = result.length;
+        while (match.pop()) {
+            matchLength = match.length;
             // find alternate
-            path = alters[matchLength];
+            path = paths[matchLength];
             // remove alternate (used)
-            alters[matchLength] = null;
+            paths[matchLength] = null;
 
-            if (path) {
-                // alternate found
-                matchIndex = path.valueIndex;
-                ruleIndex = path.ruleIndex;
-
-                if (path.value) {
-                    result.push(path.value);
-                }
-
-                break;
+            if (!path) {
+                continue;
             }
+
+            // alternate found
+            matchIndex = path.valueIndex;
+            rulesIndex = path.ruleIndex;
+
+            if (path.value) {
+                match.push(path.value);
+            }
+
+            break;
         }
 
         // how to avoid double `if (alter)` with no labels?
@@ -105,9 +102,9 @@ function matchValues(rules, types, values) {
             return null;
         }
 
-    } while ((ruleIndex += 1) < rulesLength);
+    } while ((rulesIndex += 1) < rulesLength);
 
-    return result;
+    return match;
 }
 
 module.exports = matchValues;
