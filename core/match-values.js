55befe384ffef6d1bf32ee8d11929bcef6047643
golyshevd <golyshev.dmitry@yandex.ru>
2015-03-19 02:47:58 +0300

    Refactor matchValues
    * Avoid nested loops
    * Reduce blocks depth

--- core/match-values.js
+++ core/match-values.js
@@ -4,11 +4,10 @@ function Path(rulesIndex, valueIndex, matchLength) {
     this.rulesIndex = rulesIndex;
     this.valueIndex = valueIndex;
     this.matchLength = matchLength;
-    this.value = '';
 }
 
 function matchValues(rules, kinds, value) {
-    /*eslint max-depth: 0, no-constant-condition: 0, complexity: 0*/
+    /*eslint complexity: 0*/ // 11 now =(
     var kind = void 0;
     var path = null;
     var rule = void 0;
@@ -17,86 +16,63 @@ function matchValues(rules, kinds, value) {
     var matchCount = 0;
     var matchIndex = -1;
     var rulesIndex = 0;
-    var valueIndex = matchIndex;
+    var valueIndex = 0;
     var valueLength = value.length;
     var rulesLength = rules.length;
 
-    do {
-        // match current rule
+    while (rulesIndex < rulesLength) {
         rule = rules[rulesIndex];
         kind = kinds[rule.kind];
-        matchCount = 0;
-        valueIndex = matchIndex;
 
-        while (valueLength - valueIndex > 1) {
-            // match all values to rule
-            valueIndex += 1;
+        if (valueIndex < valueLength) {
 
             if (!kind.check(value[valueIndex])) {
-                // the value is not suitable
+                valueIndex += 1;
                 continue;
             }
 
-            // Score! We have a match!
             matchCount += 1;
 
-            if (rule.required) {
-                if (matchCount > 1) {
-                    // every optional match in required rule is an alternate path
-                    // save link to previous match
-                    paths.push(new Path(rulesIndex, matchIndex, match.length));
-                }
-            } else {
-                // every match on optional rule is an alternate path
-                path = new Path(rulesIndex, matchIndex, match.length);
-
-                if (matchCount === 1) {
-                    // save default value for the case when alternate is to skip the rule
-                    path.value = rule.value;
-                }
-
-                paths.push(path);
+            if (!rule.required || matchCount > 1) {
+                paths[paths.length] = new Path(rulesIndex, matchIndex, match.length);
             }
-            // Save result
-            match.push(value[valueIndex]);
-            // save success match index
+
+            match[match.length] = value[valueIndex];
             matchIndex = valueIndex;
+            valueIndex += 1;
 
-            if (!rule.multiple) {
-                // rule supports single match, stop matching rule
-                break;
+            if (rule.multiple) {
+                continue;
             }
-            // the rule supports for multiple matches
         }
 
-        if (matchCount > 0) {
-            // matches found, jump to next rule
+        if (matchCount) {
+            matchCount = 0;
+            valueIndex = matchIndex + 1;
+            rulesIndex += 1;
             continue;
         }
 
         if (rule.required) {
-            // rule is required, need to find alternate path
+
             if (!paths.length) {
-                // no alternates
                 return null;
             }
 
             path = paths.pop();
-
-            // alternate found, unpack state
             matchIndex = path.valueIndex;
             rulesIndex = path.rulesIndex;
-            rule = rules[rulesIndex];
-            // crop match result
             match.length = path.matchLength;
+            rule = rules[rulesIndex];
         }
 
         if (rule.value) {
-            // have value with alternate
-            match.push(rule.value);
+            match[match.length] = rule.value;
         }
 
-    } while ((rulesIndex += 1) < rulesLength);
+        valueIndex = matchIndex + 1;
+        rulesIndex += 1;
+    }
 
     return match;
 }
